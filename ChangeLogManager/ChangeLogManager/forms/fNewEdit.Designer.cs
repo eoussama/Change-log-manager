11fef851292d0685359e493dd32433a37c2d6165
Oussama <Oussama_gtacj@live.fr>
2018-01-02 12:14:29 +0000

    Add files via upload

---
+++ ChangeLogManager/ChangeLogManager/forms/fNewEdit.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ChangeLogManager.forms
+{
+    partial class fNewEdit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fNewEdit));
+            this.bEdit = new System.Windows.Forms.Button();
+            this.tbEdit = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bEdit
+            // 
+            this.bEdit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bEdit.Enabled = false;
+            this.bEdit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.bEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bEdit.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bEdit.Location = new System.Drawing.Point(114, 27);
+            this.bEdit.Name = "bEdit";
+            this.bEdit.Size = new System.Drawing.Size(88, 32);
+            this.bEdit.TabIndex = 3;
+            this.bEdit.Text = "Edit";
+            this.bEdit.UseVisualStyleBackColor = true;
+            this.bEdit.Click += new System.EventHandler(this.bEdit_Click);
+            // 
+            // tbEdit
+            // 
+            this.tbEdit.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.tbEdit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbEdit.Font = new System.Drawing.Font("Bahnschrift", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEdit.ForeColor = System.Drawing.Color.White;
+            this.tbEdit.Location = new System.Drawing.Point(0, 0);
+            this.tbEdit.Name = "tbEdit";
+            this.tbEdit.Size = new System.Drawing.Size(316, 22);
+            this.tbEdit.TabIndex = 2;
+            this.tbEdit.TextChanged += new System.EventHandler(this.tbEdit_TextChanged);
+            // 
+            // fNewEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 65);
+            this.Controls.Add(this.bEdit);
+            this.Controls.Add(this.tbEdit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fNewEdit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Change-log edit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bEdit;
+        private System.Windows.Forms.TextBox tbEdit;
+    }
+}
