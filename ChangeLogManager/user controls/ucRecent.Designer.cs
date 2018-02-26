eedf894fa962f207d3033824ade618f823ea704e
Oussama <Oussama_gtacj@live.fr>
2018-02-26 12:35:34 +0000

    Add files via upload
    + Welcome form added.
    + The ability to edit the title and the version of change-logs.
    + Better formatting for HTML and Markdown.
    + Tons of bug fixes.

---
+++ ChangeLogManager/user controls/ucRecent.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ChangeLogManager
+{
+    partial class ucRecent
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lTitle = new System.Windows.Forms.Label();
+            this.lVersion = new System.Windows.Forms.Label();
+            this.lDate = new System.Windows.Forms.Label();
+            this.pbIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lTitle
+            // 
+            this.lTitle.AutoSize = true;
+            this.lTitle.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTitle.Location = new System.Drawing.Point(54, 3);
+            this.lTitle.Name = "lTitle";
+            this.lTitle.Size = new System.Drawing.Size(65, 13);
+            this.lTitle.TabIndex = 1;
+            this.lTitle.Text = "Update Title";
+            // 
+            // lVersion
+            // 
+            this.lVersion.AutoSize = true;
+            this.lVersion.Font = new System.Drawing.Font("Bahnschrift", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lVersion.Location = new System.Drawing.Point(54, 20);
+            this.lVersion.Name = "lVersion";
+            this.lVersion.Size = new System.Drawing.Size(58, 13);
+            this.lVersion.TabIndex = 2;
+            this.lVersion.Text = "Version 0.1";
+            // 
+            // lDate
+            // 
+            this.lDate.AutoSize = true;
+            this.lDate.Font = new System.Drawing.Font("Bahnschrift Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lDate.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lDate.Location = new System.Drawing.Point(54, 36);
+            this.lDate.Name = "lDate";
+            this.lDate.Size = new System.Drawing.Size(45, 13);
+            this.lDate.TabIndex = 3;
+            this.lDate.Text = "1/1/2018";
+            // 
+            // pbIcon
+            // 
+            this.pbIcon.Image = global::ChangeLogManager.Properties.Resources.iconfiles;
+            this.pbIcon.Location = new System.Drawing.Point(3, 3);
+            this.pbIcon.Name = "pbIcon";
+            this.pbIcon.Size = new System.Drawing.Size(45, 45);
+            this.pbIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIcon.TabIndex = 0;
+            this.pbIcon.TabStop = false;
+            // 
+            // ucRecent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.Controls.Add(this.lDate);
+            this.Controls.Add(this.lVersion);
+            this.Controls.Add(this.lTitle);
+            this.Controls.Add(this.pbIcon);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "ucRecent";
+            this.Size = new System.Drawing.Size(167, 54);
+            this.MouseEnter += new System.EventHandler(this.ucRecent_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.ucRecent_MouseLeave);
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbIcon;
+        private System.Windows.Forms.Label lTitle;
+        private System.Windows.Forms.Label lVersion;
+        private System.Windows.Forms.Label lDate;
+    }
+}
---
+++ ChangeLogManager/user controls/ucRecent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeLogManager
+{
+    public partial class ucRecent : UserControl
+    {
+        public static Label title, version, date;
+        public static string path;
+
+        public ucRecent()
+        {
+            InitializeComponent();
+
+            title = lTitle;
+            version = lVersion;
+            date = lDate;
+        }
+
+        private void ucRecent_MouseEnter(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Silver;
+        }
+
+        private void ucRecent_MouseLeave(object sender, EventArgs e)
+        {
+            this.BackColor = SystemColors.ScrollBar;
+        }
+    }
+}
